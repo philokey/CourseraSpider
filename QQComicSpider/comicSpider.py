1a8d56d25d167ed85176ee7e92ec9a4d824b7787
philokey <philokeys@gmail.com>
2015-07-16 18:44:18 +0800

    Tencent Comic Spider

---
+++ QQComicSpider/comicSpider.py
@@ -0,0 +1,13 @@
+import requests
+import re
+
+class ComicSpider(object):
+	"""docstring for comicSpider"""
+	def __init__(self, cid, ):
+		self.arg = arg
+		
+
+def main():
+
+if __name__ == '__main__':
+	main()
